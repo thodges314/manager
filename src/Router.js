8beb1b924886282477e6e00dbbda788376c56a71
Thomas Hodges <thodges@javascriptonline.org>
2019-06-24 14:49:52 -0700

    updated to scene/stack

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Scene, Router, Actions} from 'react-native-router-flux'
+import {Scene, Stack, Router, Actions} from 'react-native-router-flux'
 import EmployeeCreate from './components/EmployeeCreate'
 import EmployeeList from './components/EmployeeList'
 import LoginForm from './components/LoginForm'
@@ -7,25 +7,26 @@ import LoginForm from './components/LoginForm'
 const RouterComponent = () => {
 	return (
 		<Router>
-			<Scene key='root' hideNavBar>
-				<Scene key="auth">
+			<Stack key='root' hideNavBar>
+				<Stack key="auth" initial>
 					<Scene key='login' component={LoginForm} title = 'Please log in.' initial />
-				</Scene>
-				<Scene key="main">
+				</Stack>
+				<Stack key="main">
 					<Scene
 						key='employeeList'
 						component={EmployeeList}
 						title='Employees'
 						rightTitle='add'
 						onRight={()=>Actions.employeeCreate()}
+						initial
 					/>
 					<Scene 
 						key='employeeCreate'
 						component={EmployeeCreate}
 						title='Create Employee'
 					/>
-				</Scene>
-			</Scene>
+				</Stack>
+			</Stack>
 		</Router>
 	)
 }
