b25221bf8d5ed69e9a56d5e8ee390802c47b4b11
Thomas Hodges <thodges@javascriptonline.org>
2019-06-24 18:28:15 -0700

    redux wired in

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react'
-// import {View, Text} from 'react-native'
-import{Card, CardSection, Input, Button} from './common'
+import {connect} from 'react-redux'
+import {employeeUpdate} from '../actions'
+import {Card, CardSection, Input, Button} from './common'
 
 class EmployeeCreate extends Component {
 	render () {
+		const {employeeUpdate, empName, telephoneNumber} = this.props
+		console.log(empName, telephoneNumber)
 		return (
 			<Card>
 				<CardSection>
 					<Input
 						label='name'
+						onChangeText={value => employeeUpdate({prop: 'empName', value})}
 						placeholder='Jane'
+						value = {empName}
 					/>
 				</CardSection>
 				<CardSection>
 					<Input
 						label='telephone number'
+						onChangeText={value => employeeUpdate({prop: 'telephoneNumber', value})}
 						placeholder='(###)###-####'
+						value={telephoneNumber}
 					/>
 				</CardSection>
 				<CardSection></CardSection>
@@ -27,4 +34,9 @@ class EmployeeCreate extends Component {
 	}
 }
 
-export default EmployeeCreate
+const mapStateToProps = (state) => {
+	const{empName, telephoneNumber, shift} = state.employeeForm
+	return {empName, telephoneNumber, shift}
+}
+
+export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate)
--- src/reducers/employeeFormReducer.js
+++ src/reducers/employeeFormReducer.js
@@ -3,6 +3,7 @@ import {EMPLOYEE_UPDATE} from '../constants/types/'
 const INITIAL_STATE={}
 
 export default (state = INITIAL_STATE, action) => {
+	console.log(action)
 	switch(action.type) {
 		case EMPLOYEE_UPDATE:
 			//action.payload === {prop: 'name', value: 'jane'}
