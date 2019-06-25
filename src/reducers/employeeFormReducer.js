d8a811fd068eb9b8d3d86d5c517590d8ac49ab6c
Thomas Hodges <thodges@javascriptonline.org>
2019-06-24 18:00:46 -0700

    redux wired for employee - also refactored

--- src/reducers/employeeFormReducer.js
+++ src/reducers/employeeFormReducer.js
@@ -1,9 +1,12 @@
-import {EMPLOYEE_UPDATE} from '..constants/types/'
+import {EMPLOYEE_UPDATE} from '../constants/types/'
 
 const INITIAL_STATE={}
 
 export default (state = INITIAL_STATE, action) => {
 	switch(action.type) {
+		case EMPLOYEE_UPDATE:
+			//action.payload === {prop: 'name', value: 'jane'}
+			return {...state, [action.payload.prop]: action.payload.value}	// key interpolation
 		default:
 			return state
 	}
